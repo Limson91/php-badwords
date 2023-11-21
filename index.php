c616be4cc611a926bc6dd46158e7475f4fca68fe
Limson91 <matteo.a.colautti@gmail.com>
2023-11-21 16:45:33 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords</title>
+</head>
+<body>
+    <form action=“replace_word.php” method=“POST”>
+	    <input type=“text” name=“replace-word” placeholder="Insert word to replace...">
+	    <input type="submit" value="Send">
+    </form>
+</body>
+</html>
